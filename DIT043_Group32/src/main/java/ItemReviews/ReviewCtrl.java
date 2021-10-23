059183ff33dc82b4c12b0267be5ba3591838e426
Marwasel <75786866+Marwasel@users.noreply.github.com>
2021-10-23 21:18:36 +0200

    Add files via upload

--- DIT043_Group32/src/main/java/ItemReviews/ReviewCtrl.java
+++ DIT043_Group32/src/main/java/ItemReviews/ReviewCtrl.java
@@ -1,264 +1,251 @@
 package ItemReviews;
 
-import itemStore.Item;
-import itemStore.ItemCtrl;
-
 import java.text.DecimalFormat;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class ReviewCtrl {
 
+    private HashMap<String, ArrayList<Review>> reviewMap = new HashMap<String, ArrayList<Review>>();
 
-    private ArrayList<Reviews> reviewList = new ArrayList<Reviews>();
 
-    Reviews reviewAccess = new Reviews();
-    ItemCtrl ItemCtrlAccess = new ItemCtrl();
-    Item itemAccess = new Item();
 
-    //Method is not working...
+    public int getItemsReviewsSize() {
+        return reviewMap.size();
+    }
+
+    public int maxReviewsByItem = 0;
+    public int leastReviewsByItem = 0;
+
+
     public String reviewItem(String itemID, String reviewComment, int reviewGrade) {
-        String reviewAdd = "";
-        ArrayList reviewList = new ArrayList();
+        if (0 < reviewGrade && reviewGrade <= 5) {
+            Review review = new Review(itemID, reviewComment, reviewGrade);
 
-        if (ItemCtrlAccess.itemList.contains(itemID) || !reviewComment.isEmpty() || reviewGrade <= 5 || reviewGrade >= 0) {
-            Reviews review = new Reviews(itemID, reviewComment, reviewGrade);
-            reviewList.add(review);
-            reviewAdd = "Your item review was registered successfully.";
-            System.out.println(itemID + ":" + itemAccess.getItemName() + "." + itemAccess.getUnitPrice() + ".");
+            if (reviewMap.containsKey(itemID)) {
+                ArrayList reviews = reviewMap.get(itemID);
+                reviews.add(review);
+            }
 
-
-        } else if (!ItemCtrlAccess.itemList.contains(itemID) || reviewComment.isEmpty() || reviewGrade < 0 || reviewGrade > 5) {
-            reviewAdd = "Invalid data for item.";
-
+            else {
+                ArrayList reviews = new ArrayList<Review>();
+                reviews.add(review);
+                reviewMap.put(itemID, reviews);
+            }
+            return "Your item review was registered successfully.";
         }
-        return reviewAdd;
-
+        return "Grade values must be between 1 and 5.";
     }
 
-
-    private boolean invalidIndex(ArrayList<Reviews> reviewList, int index) {
-        if (index < 0 || index > reviewList.size()) {
-        }
-        return false;
-    }
-
-    private boolean validIndex(ArrayList<Reviews> reviewList, int index) {
-        if (index > 0 || index <= reviewList.size()) {
-        }
-        return true;
+    private boolean isValidIndex(String itemID, int index) {
+        ArrayList reviews = reviewMap.get(itemID);
+        return (index > 0 || index <= reviews.size());
     }
 
     public String getPrintedItemReview(String itemID, int reviewNumber) {
-        int index = 0;
-        String review = "";
-        if (reviewList.contains(itemID) && validIndex(reviewList, index)) {
-            review = "Grade:" + reviewAccess.getReviewGrade() + "." + reviewAccess.getReviewComment();
-
-        } else if (!reviewList.contains(itemID)) {
-            review = "Item<ID> " + itemID + " was not registered yet.”";
-
-        } else if (reviewList.contains(itemID) || reviewNumber == 0) {
-            review = " Item " + itemID + "Has not been reviewed yet.";
-
-        } else if (reviewList.contains(itemID) && invalidIndex(reviewList, index))
-            review = "Invalid review number.Choose between 1 and " + reviewList.size();
-
-        return review;
+        if (!reviewMap.containsKey(itemID)) {
+            return " Item " + itemID + "Has not been reviewed yet.";
+        }
+        if (!isValidIndex(itemID, reviewNumber)) {
+            return "Invalid review number.Choose between 1 and " + reviewMap.get(itemID).size();
+        }
+        Review review = reviewMap.get(itemID).get(reviewNumber);
+        return review.toString();
     }
+
 
     public String getPrintedReviews(String itemID) {
 
+        if (!reviewMap.containsKey(itemID)) {
+            return " Item " + itemID + " has not been reviewed yet.";
+        }
         String printedReview = "";
 
-        for (Reviews review : reviewList) {
-            printedReview = reviewAccess.toString() + "\n" +
-                    "Grade:" + reviewAccess + "." + reviewAccess;
-        }
-        if (!reviewList.contains(itemID)) {
-            printedReview = "Item id: " + itemID + " was not registered yet.";
-        }
-        return printedReview;
 
+
+        for (Review review : reviewMap.get(itemID)) {
+            printedReview += review.toString() + "\n";
+        }
+
+        return printedReview;
     }
 
 
     public double getItemMeanGrade(String itemID) {
-
         double sum = 0.0;
         double meanGrade = 0.0;
 
-        if (!reviewList.contains(itemID)) {
-            System.out.println("Item<ID> " + itemID + " was not registered yet.");
-
-        } else if (reviewList.contains(itemID) || reviewAccess.getReviewNumber() == 0) {
-            System.out.println(" Item " + itemID + "Has not been reviewed yet.");
-
-        } else {
-            for (Reviews review : reviewList) {
-                sum += review.getReviewGrade();
-            }
+        if (!reviewMap.containsKey(itemID)) {
+            System.out.println(" Item " + itemID + " has not been reviewed yet.");
+            return -1;
         }
-        double meanValue = sum / reviewList.size();
+
+
+        for (Review review : reviewMap.get(itemID)) {
+            sum += review.getReviewGrade();
+        }
+
+        double meanValue = sum / reviewMap.get(itemID).size();
         new DecimalFormat("#.#").format(meanValue);
         return meanGrade;
     }
 
+    public List<String> getItemComments(String itemID) {
+        List<String> comments = new ArrayList<String>();
 
-    public String getItemCommentsPrinted(String itemID) {
 
-        String comments = "";
+        if (!reviewMap.containsKey(itemID)) {
+            System.out.println(" Item " + itemID + " has not been reviewed yet.");
+            return comments;
+        }
 
-        if (reviewAccess.getReviewComment().isEmpty()) {
-            comments = null;
-        } else if (!reviewList.contains(itemID)) {
-            comments = "Item<ID> " + itemID + " was not registered yet.”";
 
-        } else
-            for (Reviews review : reviewList)
-                comments = "Comments:" + reviewAccess.getReviewComment();
-
+        for (Review review : reviewMap.get(itemID)) {
+            comments.add(review.getReviewComment());
+        }
         return comments;
     }
 
 
-    public String printAllReviews() {                            //Wrong method. This might print only one review??
-
-        ArrayList<Reviews> reviewList = new ArrayList<Reviews>();
-
-        String printReviews = "";
-
-        if (reviewAccess.reviewNumber == 0) {
-            printReviews = "No items were reviewed yet";
-        } else if (!reviewList.contains(reviewAccess.getItemID())) {
-            printReviews = "No Items registered yet.”";
-
-        } else
-            for (int i = 0; i < reviewList.size(); i++) {
-                System.out.println(reviewList.get(i));
-                for (Reviews review : reviewList) {
-                    printReviews = "Review(s) for " + review.itemID + ":" + itemAccess.itemName
-                            + ". " + itemAccess.unitPrice + "." + "\n" + review;
-                }
-            }
-        return printReviews;
-
-    }
-
-
     public int getNumberOfReviews(String itemID) {
-        for (Reviews reviews : reviewList) ;
-        int reviewCount = reviewList.size();
 
-
-        return reviewCount;
-    }
-
-    //Not needed?
-    public void sort() {
-
-        List<String> reviewList = new ArrayList();
-        Collections.sort(reviewList);
-        for (String sortedList : reviewList) {
-            System.out.println(sortedList);
+        if (!reviewMap.containsKey(itemID)) {
+            System.out.println(" Item " + itemID + " has not been reviewed yet.");
+            return 0;
         }
+        return reviewMap.get(itemID).size();
     }
 
 
-    // Is it the right method to find the most reviewed items?
-    public List<String> getMostReviewedItems() {
+    public List<String> getMostReviewedItemID() {
+        HashMap<String, Integer> reviewdItemCount = new HashMap<String, Integer>();
+        ArrayList<String> maxItemIDs = new ArrayList<String>();
+        int maxReviews = 0;
+        int reviewCount = 0;
 
-
-        ArrayList<String> mostFrequentList = new ArrayList<String>(reviewAccess.getReviewNumber());
-
-        if (!reviewList.contains(reviewAccess.getItemID())) {
-            System.out.println("No items registered yet.");
-
-        } else if (reviewList.contains(reviewAccess.getItemID()) || reviewAccess.getReviewNumber() == 0) {
-            System.out.println("No items were reviewed yet.");
-
-        } else
-            System.out.println("Most reviews: " + Collections.max(mostFrequentList));
-        {
-
+        // Iterate over the reviews map
+        for (String itemID : reviewMap.keySet()) {
+            // If there are reviews before
+            if (reviewdItemCount.containsKey(itemID)) {
+                int count = reviewdItemCount.get(itemID);
+                count++;
+            }
+            // If this is the first time
+            else {
+                reviewdItemCount.put(itemID, 1);
+            }
         }
 
-        return mostFrequentList;
-    }
-
-
-    //What is the difference here from the getMostReviewedItems method? And is it valid to do it this way??
-    public String printMostReviewedItems() {
-
-        return "Most reviewed:" + Collections.max(getMostReviewedItems()) + "review(s) each" + "\n"
-                + itemAccess.toString();
-    }
-
-
-    public List<String> getLeastReviewedItems() {
-
-        ArrayList<String> leastFrequentList = new ArrayList<String>(reviewAccess.getReviewNumber());
-
-        if (!reviewList.contains(reviewAccess.getItemID())) {
-            System.out.println("No items registered yet.");
-
-        } else if (reviewList.contains(reviewAccess.getItemID()) || reviewAccess.getReviewNumber() == 0) {
-            System.out.println("No items were reviewed yet.");
-
-        } else
-            System.out.println("Least Reviews: " + Collections.min(leastFrequentList));
-
-        return leastFrequentList;
-    }
-
-
-    public String printLeastReviewedItems() {
-
-        return "Least reviewed:" + Collections.min(getLeastReviewedItems()) + "review(s) each" + "\n"
-                + itemAccess.toString();
-
-    }
-
-//Wrong method
-    public List<String> getBestReviewedItems() {
-
-        ArrayList<String> bestReviewList = new ArrayList<String>();
-        int sum = 0;
-
-        if (!reviewList.contains(reviewAccess.getItemID())) {
-            System.out.println("No items registered yet.");
-
-        } else if (reviewList.contains(reviewAccess.itemID) || reviewAccess.getReviewNumber() == 0) {
-            System.out.println("No Items were reviewed yet.");
-
-        } else {
-            System.out.println("Items with best mean reviews: " + Collections.max(bestReviewList));
-            double average = sum / bestReviewList.size();
+        // Get top review
+        for (String itemID : reviewdItemCount.keySet()) {
+            reviewCount = reviewdItemCount.get(itemID);
+            if (maxReviews < reviewCount) {
+                maxReviews = reviewCount;
+            }
         }
 
-        return null;
-    }
 
-//Wrong method
-    public List<String> getWorseReviewedItems() {
-
-        ArrayList<String> worstReviewList = new ArrayList<String>(reviewAccess.getReviewGrade());
-        int sum = 0;
-
-        if (!reviewList.contains(reviewAccess.getItemID())) {
-            System.out.println("No items registered yet.");
-
-        } else if (reviewList.contains(reviewAccess.itemID) || reviewAccess.getReviewNumber() == 0) {
-            System.out.println("No Items were reviewed yet.");
-
-        } else {
-            System.out.println("Items with best mean reviews: " + Collections.min(worstReviewList));
-            double average = sum / worstReviewList.size();
+        // Get top Items
+        for (String itemID : reviewdItemCount.keySet()) {
+            reviewCount = reviewdItemCount.get(itemID);
+            if (maxReviews == reviewCount) {
+                maxItemIDs.add(itemID);
+            }
         }
-        return null;
+
+        this.maxReviewsByItem = maxReviews;
+        return maxItemIDs;
     }
-}
+
+
+    public List<String> getLeastReviewedItemID() {
+        HashMap<String, Integer> reviewdItemCount = new HashMap<String, Integer>();
+        ArrayList<String> leastItemIDs = new ArrayList<String>();
+        int leastReviews = 100;
+        int reviewCount = 0;
+
+
+        for (String itemID : reviewMap.keySet()) {
+
+            if (reviewdItemCount.containsKey(itemID)) {
+                int count = reviewdItemCount.get(itemID);
+                count++;
+            }
+            // for the first review?
+            else {
+                reviewdItemCount.put(itemID, 1);
+            }
+        }
+        //Then to be able to get the least review...
+        for (String itemID : reviewdItemCount.keySet()) {
+            reviewCount = reviewdItemCount.get(itemID);
+            if (reviewCount < leastReviews) {
+                leastReviews = reviewCount;
+            }
+        }
+
+        
+        for (String itemID : reviewdItemCount.keySet()) {
+            reviewCount = reviewdItemCount.get(itemID);
+            if (leastReviews == reviewCount) {
+                leastItemIDs.add(itemID);
+            }
+        }
+        this.leastReviewsByItem = leastReviews;
+        return leastItemIDs;
+    }
+
+    public String printAllReviews() {
+        String printedReview = "";
+
+
+        for (String itemID : reviewMap.keySet()) {
+            for (Review review : reviewMap.get(itemID)) {
+                printedReview += review.toString() + "\n";
+            }
+        }
+        return printedReview;
+    }
+
+  //  public List<String> getBestReviewedItems() {
+
+   //    ArrayList<String> bestReviewList = new ArrayList<String>();
+   //     int sum = 0;
+
+  //     if (!reviewList.contains(reviewAccess.getItemID())) {
+  //          System.out.println("No items registered yet.");
+
+  //     } else if (reviewList.contains(reviewAccess.itemID) || reviewAccess.getReviewNumber() == 0) {
+  //          System.out.println("No Items were reviewed yet.");
+//
+//        } else {
+//            System.out.println("Items with best mean reviews: " + Collections.max(bestReviewList));
+//            double average = sum / bestReviewList.size();
+//        }
+//
+//        return null;
+//    }
+//
+////Wrong method
+//    public List<String> getWorseReviewedItems() {
+//
+//        ArrayList<String> worstReviewList = new ArrayList<String>(reviewAccess.getReviewGrade());
+//        int sum = 0;
+//
+//        if (!reviewList.contains(reviewAccess.getItemID())) {
+//            System.out.println("No items registered yet.");
+//
+//        } else if (reviewList.contains(reviewAccess.itemID) || reviewAccess.getReviewNumber() == 0) {
+//            System.out.println("No Items were reviewed yet.");
+//
+//        } else {
+//            System.out.println("Items with best mean reviews: " + Collections.min(worstReviewList));
+//            double average = sum / worstReviewList.size();
+//        }
+//        return null;
+//    }
+    }
+
+
 
 
 
