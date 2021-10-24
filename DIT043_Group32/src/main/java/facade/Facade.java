81694cf350a9dced159a631e0e7c39171b0ec285
Arezoo Allahyari <92451123+gusallaar@users.noreply.github.com>
2021-10-25 00:22:27 +0200

    Update Facade.java

--- DIT043_Group32/src/main/java/facade/Facade.java
+++ DIT043_Group32/src/main/java/facade/Facade.java
@@ -313,41 +313,293 @@ public class Facade {
 
 
 
-    public String createEmployee(String employeeID, String employeeName, double grossSalary) throws Exception {
-        return "";
+      public String createEmployee(String EmpID, String EmpName, double GrossSalary) throws Exception {
+        GrossSalary = Math.round(GrossSalary*100.0)/100.0;
+        double NetSalary_unround = GrossSalary-0.1*GrossSalary;
+        double NetSalary = Math.round(NetSalary_unround*100)/100;
+
+            String createResult="";
+            if (EmpID.isEmpty() || EmpName.isEmpty() || GrossSalary <= 0) {
+                createResult= "Invalid data for item.";
+            } else {
+                EmpReg emp = new EmpReg(EmpID, EmpName, GrossSalary,NetSalary);
+                EmpRegList.add(emp);
+
+                createResult="Employee "+EmpID+" was registered successfully.";
+
+            }
+            return createResult;
+
+
     }
 
-    public String printEmployee(String employeeID) throws Exception {
-        return "";
+    public String printEmployee(String employeeID) {
+        String messageToPrint="";
+        EmpReg EmpRegToPrint=null;
+        EmpDir EmpDirToPrint=null;
+        EmpMan EmpManToPrint=null;
+        EmpInt EmpIntToPrint=null;
+        for(EmpReg currentEmp: EmpRegList){
+            if(currentEmp.getEmpID().equals(employeeID)){
+                EmpRegToPrint=currentEmp;
+            }
+        }
+
+        for(EmpDir currentEmp: EmpDirList){
+            if(currentEmp.getEmpID().equals(employeeID)){
+                EmpDirToPrint=currentEmp;
+            }
+        }
+        for(EmpMan currentEmp: EmpManList){
+            if(currentEmp.getEmpID().equals(employeeID)){
+                EmpManToPrint=currentEmp;
+            }
+        }
+        for(EmpInt currentEmp: EmpIntList){
+            if(currentEmp.getEmpID().equals(employeeID)){
+                EmpIntToPrint=currentEmp;
+            }
+        }
+
+        if(EmpRegToPrint == null && EmpDirToPrint == null && EmpManToPrint == null && EmpIntToPrint == null){
+            messageToPrint= "Employee " +employeeID+ " was not registered yet.";
+        }else if(EmpRegToPrint != null){
+            messageToPrint=EmpRegToPrint.toString();
+        }else if(EmpManToPrint != null){
+            messageToPrint=EmpManToPrint.toString();
+        }else if(EmpDirToPrint != null){
+            messageToPrint=EmpDirToPrint.toString();
+        }else if(EmpIntToPrint != null){
+            messageToPrint=EmpIntToPrint.toString();
+        }
+        return messageToPrint;
     }
 
-    public String createEmployee(String employeeID, String employeeName, double grossSalary, String degree) throws Exception {
-        return "";
+    public String createEmployee(String employeeID, String employeeName, double GrossSalary, String degree) throws Exception {
+
+
+
+        switch (degree) {
+            case "BSc":
+                GrossSalary = GrossSalary*1.1;
+                break;
+            case "MSc":
+
+                GrossSalary = GrossSalary*1.2;
+                break;
+
+            case "PhD":
+                GrossSalary = GrossSalary*1.35;
+                break;
+            //
+
+        }
+        GrossSalary = Math.round(GrossSalary * 100.0) / 100.0;
+
+        double NetSalary_unround = GrossSalary - 0.1 * GrossSalary;
+        double NetSalary = Math.round(NetSalary_unround * 100.0) / 100.0;
+
+        String createResult="";
+        if (employeeID.isEmpty() || employeeName.isEmpty() || GrossSalary <= 0 || degree.equals("")) {
+            createResult= "Invalid data for item.";
+        } else {
+            EmpMan empMan = new EmpMan(employeeID, employeeName, GrossSalary,NetSalary,degree);
+            EmpManList.add(empMan);
+
+            createResult="Employee "+employeeID+" was registered successfully.";
+
+        }
+
+        return createResult;
+
     }
 
-    public String createEmployee(String employeeID, String employeeName, double grossSalary, int gpa) throws Exception {
-        return "";
+    public String createEmployee(String employeeID, String employeeName, double GrossSalary, int gpa) throws Exception {
+        GrossSalary = Math.round(GrossSalary * 100.0) / 100.0;
+        double NetSalary_unround = GrossSalary-0.1*GrossSalary;
+        double NetSalary = Math.round(NetSalary_unround*100.0)/100.0;
+
+        if (gpa<5){
+            GrossSalary=0;
+            NetSalary=0;
+
+        }else if ((gpa>5)&(gpa<8)){
+            GrossSalary=GrossSalary;
+            NetSalary=GrossSalary;
+
+        }else if (gpa>8){
+            GrossSalary=GrossSalary+1000;
+            NetSalary=GrossSalary;
+        }
+        String createResult="";
+        if (employeeID.isEmpty() || employeeName.isEmpty() || GrossSalary <= -1) {
+            createResult= "Invalid data for item.";
+        } else {
+            EmpInt empInt = new EmpInt(employeeID, employeeName, GrossSalary,NetSalary,gpa);
+            EmpIntList.add(empInt);
+
+            createResult="Employee "+employeeID+" was registered successfully.";
+
+        }
+
+        return createResult;
     }
 
     public double getNetSalary(String employeeID) throws Exception {
-        return -1.0;
+
+        Double NetSalaryToPrint=null;
+        EmpReg EmpRegToPrint=null;
+        EmpDir EmpDirToPrint=null;
+        EmpMan EmpManToPrint=null;
+        EmpInt EmpIntToPrint=null;
+        for(EmpReg currentEmp: EmpRegList){
+            if(currentEmp.getEmpID().equals(employeeID)){
+                EmpRegToPrint=currentEmp;
+                NetSalaryToPrint=EmpRegToPrint.getNetSalary();
+            }
+        }
+        for(EmpDir currentEmp: EmpDirList){
+            if(currentEmp.getEmpID().equals(employeeID)){
+                EmpDirToPrint=currentEmp;
+                NetSalaryToPrint=EmpDirToPrint.getNetSalary();
+            }
+        }
+        for(EmpMan currentEmp: EmpManList){
+            if(currentEmp.getEmpID().equals(employeeID)){
+                EmpManToPrint=currentEmp;
+                NetSalaryToPrint=EmpManToPrint.getNetSalary();
+            }
+        }
+        for(EmpInt currentEmp: EmpIntList){
+            if(currentEmp.getEmpID().equals(employeeID)){
+                EmpIntToPrint=currentEmp;
+                NetSalaryToPrint=EmpIntToPrint.getNetSalary();
+            }
+        }
+
+
+
+        return NetSalaryToPrint;
     }
 
-    public String createEmployee(String employeeID, String employeeName, double grossSalary, String degree, String dept) throws Exception {
-        return "";
+    public String createEmployee(String employeeID, String employeeName, double GrossSalary, String degree, String dept) throws Exception {
+        GrossSalary = Math.floor(GrossSalary * 100.0) / 100.0;
+
+        switch (degree) {
+            case "BSc":
+                GrossSalary = GrossSalary*1.1 +5000;
+                break;
+            case "MSc":
+
+                GrossSalary = GrossSalary*1.2 +5000;
+                break;
+
+            case "PhD":
+                GrossSalary = GrossSalary*1.35 +5000;
+                break;
+            //
+
+        }
+
+        Double NetSalary=null;
+        if (GrossSalary<30000){
+            NetSalary=GrossSalary - 0.1 * GrossSalary;
+
+        }else if ((GrossSalary>30000)&(GrossSalary<50000)){
+            NetSalary=GrossSalary - 0.2 * GrossSalary;
+
+        }else if (GrossSalary>50000){
+            NetSalary=GrossSalary - 0.2 * 30000- 0.4 * (GrossSalary-30000);
+        }
+
+
+        String createResult="";
+        if (employeeID.isEmpty() || employeeName.isEmpty() || GrossSalary <= 0 || degree.equals("")) {
+            createResult= "Invalid data for item.";
+        } else {
+            EmpDir empDir = new EmpDir(employeeID, employeeName, GrossSalary,NetSalary,degree,dept);
+            EmpDirList.add(empDir);
+
+            createResult="Employee "+employeeID+" was registered successfully.";
+
+        }
+
+        return createResult;
     }
 
-    public String removeEmployee(String empID) throws Exception {
-        return "";
+    public String removeEmployee(String employeeID) throws Exception {
+
+        String messageToPrint="";
+        EmpReg EmpRegToPrint=null;
+        EmpDir EmpDirToPrint=null;
+        EmpMan EmpManToPrint=null;
+        EmpInt EmpIntToPrint=null;
+        for(EmpReg currentEmp: EmpRegList){
+            if(currentEmp.getEmpID().equals(employeeID)){
+                EmpRegToPrint=currentEmp;
+            }
+        }
+        for(EmpDir currentEmp: EmpDirList){
+            if(currentEmp.getEmpID().equals(employeeID)){
+                EmpDirToPrint=currentEmp;
+            }
+        }
+        for(EmpMan currentEmp: EmpManList){
+            if(currentEmp.getEmpID().equals(employeeID)){
+                EmpManToPrint=currentEmp;
+            }
+        }
+        for(EmpInt currentEmp: EmpIntList){
+            if(currentEmp.getEmpID().equals(employeeID)){
+                EmpIntToPrint=currentEmp;
+            }
+        }
+
+        if(EmpRegToPrint == null && EmpDirToPrint == null && EmpManToPrint == null && EmpIntToPrint == null){
+            messageToPrint= "Item " +employeeID+ " was not registered yet.";
+        }else if(EmpRegToPrint != null && EmpRegList.remove(EmpRegToPrint)){
+            messageToPrint="Employee "+EmpRegToPrint.getEmpID()+" was successfully removed.";
+        }else if(EmpManToPrint != null && EmpManList.remove(EmpManToPrint)){
+            messageToPrint="Employee "+EmpManToPrint.getEmpID()+" was successfully removed.";
+        }else if(EmpDirToPrint != null && EmpDirList.remove(EmpDirToPrint)){
+            messageToPrint="Employee "+EmpDirToPrint.getEmpID()+" was successfully removed.";
+        }else if(EmpIntToPrint != null && EmpIntList.remove(EmpIntToPrint)){
+            messageToPrint="Employee "+EmpIntToPrint.getEmpID()+" was successfully removed.";
+        }
+        return messageToPrint;
+
     }
 
     public String printAllEmployees() throws Exception {
-        return "";
+
+
+        String message=" All registered employees :";
+        EmpDirList.forEach(System.out::println);
+        EmpManList.forEach(System.out::println);
+        EmpIntList.forEach(System.out::println);
+        EmpRegList.forEach(System.out::println);
+
+        return message;
     }
 
     public double getTotalNetSalary() throws Exception {
-        return -1.0;
+        double totalSum = 0;
+        for(EmpInt currentEmp: EmpIntList){
+            totalSum+=currentEmp.getNetSalary();
+        }
+        for(EmpDir currentEmp: EmpDirList){
+            totalSum+=currentEmp.getNetSalary();
+        }
+        for(EmpReg currentEmp: EmpRegList){
+            totalSum+=currentEmp.getNetSalary();
+        }
+        for(EmpMan currentEmp: EmpManList){
+            totalSum+=currentEmp.getNetSalary();
+        }
+
+        return totalSum;
     }
+
 
     public String printSortedEmployees() throws Exception {
         return "";
