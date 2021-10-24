9ae84b4724bbb0b79df32b533f61d0090aa219c7
Marwasel <75786866+Marwasel@users.noreply.github.com>
2021-10-24 12:51:01 +0200

    Add files via upload

--- DIT043_Group32/src/main/java/itemStore/Item.java
+++ DIT043_Group32/src/main/java/itemStore/Item.java
@@ -1,68 +1,54 @@
 package itemStore;
 
 
-import java.util.ArrayList;
+//import java.util.ArrayList;
 
 public class Item {
 
- public ArrayList<Item> itemList = new ArrayList<Item>();
+//     public ArrayList<Item> itemList = new ArrayList<Item>();
 
-  String itemID;
- public String itemName;
- public double unitPrice ;
- public int amount;
-
-
-//Item item = new Item(itemID, itemName, unitPrice);
-
+    String itemID;
+    public String itemName;
+    public double unitPrice ;
+    public int amount;
 
     public Item(String itemID, String itemName, double unitPrice) {
         this.itemID = itemID;
         this.itemName = itemName;
         this.unitPrice = unitPrice;
-        this.amount = 0;
     }
+    public Item(String itemID, String itemName, double unitPrice, int amount) {
+        this.itemID = itemID;
+        this.itemName = itemName;
+        this.unitPrice = unitPrice;
+        this.amount = amount;
 
+    }
     public Item(){
         //Empty Constructor to get access to the class.
     }
 
+    public String getItemID(){
+        return itemID;
+    }
+    //public void setItemID(itemID){this.itemID = itemID;}
 
+    // ItemName
+    public String getItemName() {
+        return itemName;
+    }
+    public void setItemName(String itemName) { this.itemName = itemName;}
 
- public String getItemID(){
-  return itemID;
-}
+    //unitPrice
+    public double getUnitPrice() { return unitPrice;}
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
 
-public void setItemID(){
-
- this.itemID = itemID;
-}
-
-
-public String getItemName() {
-   return itemName;
-}
-
-public void setItemName() {
-
-    this.itemName = itemName;
-}
-
-public double getUnitPrice() {
-   return unitPrice;
-}
-
- public void setUnitPrice() {
-   this.unitPrice = unitPrice;
-}
-
-
-@Override
+    @Override
     public String toString() {
-
-    return itemID + ":"	 + itemName + "." + unitPrice + "SEK";
-	}
-
-
-
+        // Truncate Unit Price
+        String unitPrice = (String) String.format("%.2f", this.unitPrice);
+        return this.itemID + ": " + this.itemName + ". " + unitPrice + " SEK";
+    }
 }
